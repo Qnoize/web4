7c9709edf053581cbfb0582f2f479ad9f9cf5baf
Aleksey Danilov <alekseydanilov@MacBook-Pro-Aleksey.local>
2019-09-26 22:31:37 +0300

    Initial commit

---
+++ src/main/java/service/CarService.java
@@ -0,0 +1,28 @@
+package service;
+
+import org.hibernate.SessionFactory;
+import util.DBHelper;
+
+public class CarService {
+
+    private static CarService carService;
+
+    private SessionFactory sessionFactory;
+
+    private CarService(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    public static CarService getInstance() {
+        if (carService == null) {
+            carService = new CarService(DBHelper.getSessionFactory());
+        }
+        return carService;
+    }
+
+
+    public Object getAllCars() {
+        // заглушка
+        return getAllCars();
+    }
+}
